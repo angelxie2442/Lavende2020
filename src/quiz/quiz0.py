228dae4581a37b1b95b4a47040fe15f87579a2ac
angel <angel.xie@emory.edu>
2020-01-26 22:39:44 -0500

    Quiz 0

--- src/quiz/quiz0.py
+++ src/quiz/quiz0.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 # ========================================================================
 import numpy as np
-
 a = np.array([1,2,3])
 b = np.array([4,5,6])
 print((a + b) * 2)
