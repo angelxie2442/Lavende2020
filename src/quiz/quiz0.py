d08e71404a595c13f1083bbec9b2826d3d8e2e6a
angel <angel.xie@emory.edu>
2020-01-26 22:42:35 -0500

    Quiz 0

--- src/quiz/quiz0.py
+++ src/quiz/quiz0.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========================================================================
+
 import numpy as np
 a = np.array([1,2,3])
 b = np.array([4,5,6])
