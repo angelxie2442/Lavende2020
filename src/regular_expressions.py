f8a6b6c84d169fad1eac3f5a719638bb224578dd
angel <angel.xie@emory.edu>
2020-02-01 00:56:15 -0500

    quiz1

--- src/regular_expressions.py
+++ src/regular_expressions.py
@@ -26,7 +26,6 @@ def regex_matcher(regex: Pattern, instr: str) -> List[str]:
         for i, literal in enumerate(t):
             if ts[i] is None and literal:
                 ts[i] = literal
-
     return ts
 
 
@@ -56,7 +55,6 @@ def turn_1a(res: SimpleNamespace):
 
     duration = regex_matcher(res.re_duration, u)
     from_date = regex_matcher(res.re_from_date, u)
-
     res.in_phone_year, res.in_phone_month = None, 1
     curr_year, curr_month = 2020, 1
 
@@ -67,7 +65,11 @@ def turn_1a(res: SimpleNamespace):
             res.in_phone_year = curr_year - d
         elif m == 'month':
             res.in_phone_year = curr_year - int(d / 12)
-            res.in_phone_month = (curr_month - d) % 12
+            res.in_phone_month = curr_month - (d % 12)
+            if res.in_phone_month <= 0:
+                month_diff = abs(res.in_phone_month)
+                res.in_phone_month = 12 - month_diff
+                res.in_phone_year -= 1
     elif any(from_date):
         res.in_phone_year = int(from_date[1])
         res.in_phone_month = res.d_month_to_number[from_date[0]] if from_date[0] else 1
@@ -80,8 +82,10 @@ def turn_1a(res: SimpleNamespace):
 def turn_1b(res: SimpleNamespace):
     s = 'S: what kind of smartphone do you have?'
     u = input(s + '\nU: ')
-    # TODO: to be filled
-
+    phone_added = regex_matcher(res.re_phone, u)
+    res.in_phone_name=phone_added[1]
+    res.in_phone_company=phone_added[0]
+    turn_1a(res)
 
 def turn_1c(res: SimpleNamespace):
     s = 'really? you should consider getting one.'
@@ -89,18 +93,23 @@ def turn_1c(res: SimpleNamespace):
 
 
 def turn_2a(res: SimpleNamespace):
+    ##first make an educated guess based on from date's year
     r = res.d_iphone.get(res.in_phone_year, None)
     res.in_phone_version = None
 
-    if r:
-        v = next(models for month, models in r if month >= res.in_phone_month)
-        res.in_phone_version = v[0]
-        s = 'S: oh, are you using iphone {}?'.format(' or '.join(v))
-        u = input(s + '\nU: ')
+    if r: ##if there are phone models coming out that year, perform dictionary look up for up to that month
+        v = next((models for month, models in r if month <= res.in_phone_month), None) #iterating with next()
+        if v:
+            res.in_phone_version = v[0]
+            s = 'S: oh, are you using iphone {}?'.format(' or '.join(v))
+            u = input(s + '\nU: ')
 
-        yn = regex_matcher(res.re_yn, u)
-        if yn[1]: res.in_phone_version = None
-    else:
+            yn = regex_matcher(res.re_yn, u)
+            if yn[1]: res.in_phone_version = None ## guessed the wrong version
+        else:
+            s = 'S: which version of iphone is your model?'
+            u = input(s + '\nU: ')
+    else: ##if the from date's year is unknown
         s = 'S: which version of iphone is your model?'
         u = input(s + '\nU: ')
 
@@ -110,8 +119,7 @@ def turn_2a(res: SimpleNamespace):
 
 
 def turn_3a(res: SimpleNamespace):
-    # TODO: to be filled
-    old = 5
+    old =2020-res.d_iphone_v[res.in_phone_version]
     s = 'S: iphone {} is about {} years old'.format(res.in_phone_version, old)
     print(s)
 
@@ -123,13 +131,14 @@ if __name__ == '__main__':
     res.re_yn = re.compile(r'(?:\s|^)(yes|yeah)|(no|not really)(?:\s|,|\.|$)')
     res.re_phone = re.compile(r'(?:\s|^)(apple|google|samsung)|(iphone|pixel|galaxy|android)(?:\s|,|\.|$)')
     res.re_duration = re.compile(r'(?:\s|^)(\d+)(?:\s|-)+(month|year)')
-    res.re_from_date = re.compile(r'(?:\s|^)(?:since|from)\s(?:(january|february|march|april|may|june|july|august|september|october|november|december)\s)?(\d\d{2,4})')
+    ### always have a year, month is optional
+    res.re_from_date = re.compile(r'(?:\s|^)(?:since|from)\s(?:(january|february|march|april|may|june|july|august|september|october|november|december)\s)?(\d{2,4})')
     res.re_iphone_version = re.compile(r'(?:\s|^)(?:iphone|version)\s(\d+s?(?: (?:plus|max))?)(?:\s|,|\.|$)')
 
     # dictionaries
     res.d_month_to_number = {month: i for i, month in enumerate(['january','february','march','april','may','june','july','august','september','october','november','december'], 1)}
     res.d_iphone = {2019: [(9, ['11', '11 pro', '11 pro max'])], 2018: [(9, ['10s', '10s max'])], 2017: [(11, ['10']), (9, ['8', '8 plus'])], 2016: [(9, ['7', '7 plus'])], 2015: [(9, ['6s', '6s plus'])], 2014: [(9, ['6', '6 plus'])]}
-    res.d_iphone_v = {'11': 2019, '11 pro': 2019, '11 pro max': 2019, '10s': 2018, '10s max': 2018, '10': 2017, '8': 9, '8 plus': 9, '7': 2016, '7 plus': 2016, '6s': 9, '6s plus': 9, '6': 2014, '6 plus': 2014}
+    res.d_iphone_v = {'11': 2019, '11 pro': 2019, '11 pro max': 2019, '10s': 2018, '10s max': 2018, '10': 2017, '8': 2017, '8 plus': 2017, '7': 2016, '7 plus': 2016, '6s': 2015, '6s plus': 2015, '6': 2014, '6 plus': 2014}
 
     # run
     turn_0(res)
