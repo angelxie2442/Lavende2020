15724567664185a31295ae664e2d8b77cfc8a7dc
angel <angel.xie@emory.edu>
2020-02-16 14:12:53 -0500

    quiz3

---
+++ src/quiz/quiz3.py
@@ -0,0 +1,53 @@
+# ========================================================================
+# Copyright 2020 Emory University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
+from typing import Set, Optional, List
+from nltk.corpus import wordnet as wn
+from nltk.corpus.reader import Synset
+
+
+def antonyms(word: str, pos: Optional[str] = None) -> Set[str]:
+    ants = set()
+    for syn in wn.synsets(word, pos): ##each synset
+        if syn is not None:
+            for l in syn.lemmas(): ##each synonym of that synset
+                if l is not None:
+                    for ant in l.antonyms(): ##each antonym of that synonym
+                        if ant is not None:
+                            ants.add(ant)
+    return ants
+    pass
+
+def lch_paths(word_0: str, word_1: str) -> List[List[Synset]]:
+    paths=[]
+    ##combination of senses of word0 and word1
+    senses_0=wn.synsets(word_0)
+    senses_1=wn.synsets(word_1)
+    print(senses_1)
+    print(senses_0)
+    for syn_0 in senses_0:
+        for syn_1 in senses_1:
+            lch=syn_0.lowest_common_hypernyms(syn_1)
+            for h in lch:
+                paths.extend(h.hypernym_paths())
+    return paths
+    pass
+
+
+if __name__ == '__main__':
+    print(antonyms('man', pos='n'))
+    for path in lch_paths('dog', 'cat'):
+       print(path)
+
